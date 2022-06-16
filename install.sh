8e12303afc5033c806faad0563508940349fb10d
Ngô Viết Đức <95280687+Ngovietducc@users.noreply.github.com>
2022-06-16 12:46:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/Ngovietducc/Info./blob/main/3proxy-3proxy-0.8.6.tar.gz?raw=true"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
